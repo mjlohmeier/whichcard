13701644d85d61286518ba7dd7c7aa5aeaed7a8d
Will Davis <williammadisondavis@gmail.com>
2018-09-26 10:09:46 -0400

    action-on-click

--- src/index.js
+++ src/index.js
@@ -15,45 +15,25 @@ let initialState = {
         {"id": 2, "name": 'Ewok', "power": '2', "img": './images/jedi/ewok.jpg', "state": 'active'},
         {"id": 3, "name": 'Mace Windu', "power": '7', "img": './images/jedi/mace_windu.jpg', "state": 'active'},
         {"id": 4, "name": 'Jar Jar Binks', "power": '3', "img": './images/jedi/jarjar.jpg', "state": 'active'},
-        {"id": 5, "name": 'Aayla', "power": '10', "img": './images/jedi/aayla.jpg', "state": 'active'},
-        {"id": 6, "name": 'Chewbacca', "power": '10', "img": './images/jedi/chewy.jpg', "state": 'active'},
-        {"id": 7, "name": 'Finn', "power": '10', "img": './images/jedi/finn.jpg', "state": 'active'},
+        {"id": 5, "name": 'Aayla', "power": '4', "img": './images/jedi/aayla.jpg', "state": 'active'},
+        {"id": 6, "name": 'Chewbacca', "power": '9', "img": './images/jedi/chewy.jpg', "state": 'active'},
+        {"id": 7, "name": 'Finn', "power": '8', "img": './images/jedi/finn.jpg', "state": 'active'},
         {"id": 8, "name": 'Rebel Soldier', "power": '5', "img": './images/jedi/rebel_soldier.jpg', "state": 'active'},
-        {"id": 9, "name": 'Yoda', "power": '10', "img": './images/jedi/yoda.jpg', "state": 'active'},
-        {"id": 10, "name": 'R2-D2', "power": '10', "img": './images/jedi/r2d2.jpg', "state": 'active'}
+        {"id": 9, "name": 'Yoda', "power": '6', "img": './images/jedi/yoda.jpg', "state": 'active'},
+        {"id": 10, "name": 'R2-D2', "power": '1', "img": './images/jedi/r2d2.jpg', "state": 'active'}
     ],
-    "Sith" : {
-        '1' : {},
-        '2' : {},
-        '3' : {},
-        '4' : {},
-        '5' : {},
-        '6' : {},
-        '7' : {},
-        '8' : {},
-        '9' : {},
-        '10' : {},
-        '11' : {},
-        '12' : {},
-        '13' : {},
-        '14' : {},
-        '15' : {},
-        '16' : {},
-        '17' : {},
-        '18' : {},
-        '19' : {},
-        '20' : {},
-        '21' : {},
-        '22' : {},
-        '23' : {},
-        '24' : {},
-        '25' : {},
-        '26' : {},
-        '27' : {},
-        '28' : {},
-        '29' : {},
-        '30' : {}
-    }
+    "Sith" : [
+        {"id": 11, "name": 'Darth Vader', "power": '10', "img": './images/sith/vader.jpeg', "state": 'active'},
+        {"id": 12, "name": 'Boba Fett', "power": '9', "img": './images/sith/fett.jpeg', "state": 'active'},
+        {"id": 13, "name": 'Emperor Palpatine', "power": '7', "img": './images/sith/pp.jpg', "state": 'active'},
+        {"id": 14, "name": 'Scout Trooper', "power": '2', "img": './images/sith/scout.jpg', "state": 'active'},
+        {"id": 15, "name": 'Darth Maul', "power": '6', "img": './images/sith/maul.jpg', "state": 'active'},
+        {"id": 16, "name": 'Droideka', "power": '3', "img": './images/sith/droideka.jpg', "state": 'active'},
+        {"id": 17, "name": 'Gen. Grievous', "power": '8', "img": './images/sith/gg.jpg', "state": 'active'},
+        {"id": 18, "name": 'Kylo Ren', "power": '5', "img": './images/sith/kylo.jpg', "state": 'active'},
+        {"id": 19, "name": 'IG-100 Magnaguard', "power": '4', "img": './images/sith/ig.jpg', "state": 'active'},
+        {"id": 20, "name": 'Battle Droid', "power": '1', "img": './images/sith/droid.png', "state": 'active'}
+    ]
 };
 
 let store = createStore(
