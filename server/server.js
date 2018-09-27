19bad92552b3018e609e1da0c3632a263d99492a
William Davis <williammadisondavis@gmail.com>
2018-09-27 10:32:41 -0400

    added-server
    node express server. runs on port 3005

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+var bodyParser = require('body-parser');
+server.use(cors());
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+    res.sendStatus(200)
+});
+
+server.post('/game/id',(req, res) => {
+    let action = req.body;
+    
+    res.send(action);
+});
+
+server.listen(3005);
