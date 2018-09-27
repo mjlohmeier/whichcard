af996a858fd81a33962be50017006361a8d75845
Michael Lohmeier <mjlohmeier@gmail.com>
2018-09-27 15:29:30 -0400

    math works in console, we can make it work in the game now

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const server = express();
 var bodyParser = require('body-parser');
-server.use(cors());
+
+server.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+  });
+
+// server.use(cors());
 server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.sendStatus(200)
 });
 
-server.post('/game/id',(req, res) => {
+server.post('/game/:id',(req, res) => {
+    console.log(req.body)
     let action = req.body;
     
     res.send(action);
--- src/components/GameController.js
+++ src/components/GameController.js
@@ -1,6 +1,7 @@
 let sendAttack = (data) =>{
     fetch('http://localhost:3005/game/id', {
     method: "POST",
+    mode: 'cors',
     body: data,
     headers: {
         'Content-Type': "application/json"
@@ -9,6 +10,16 @@ let sendAttack = (data) =>{
     .then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(err => console.error('Error:', err))
+    fetch('https://my-little-cors-proxy.herokuapp.com/https://math.ly/api/v1/algebra/linear-equations.json')
+    .then(res => {
+        console.log(res)
+        return res.json()
+    })
+    .then(randommathquestion => {
+        console.log(randommathquestion)
+    })
 };
 
 export default sendAttack
+
+// fetch('http://www.wolframalpha.com/queryrecognizer/query?appid=KYAKWV-YU7YXXHKA4&mode=Default&i=RAID+array&output=json')
